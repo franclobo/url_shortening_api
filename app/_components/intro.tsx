5c9f9a2953578e1b58faa1997079c8f482018458
franclobo <fjbl2788@gmail.com>
2024-04-29 07:17:31 -0500

    REplace ' with &lsquo;

--- app/_components/intro.tsx
+++ app/_components/intro.tsx
@@ -6,7 +6,7 @@ export const Intro = () => {
     <section className="flex md:flex-row flex-col-reverse justify-center items-center md:text-left text-center gap-10 px-10 mb-20">
       <div className="flex flex-col md:items-start justify-center items-center gap-5">
         <h1 className="font-bold text-4xl md:text-6xl">More than just shorter links</h1>
-        <p className="text-gray-600 md:text-xl">Build your brand&apos;s recognition and get detailed insights on how your links are performing.</p>
+        <p className="text-gray-600 md:text-xl">Build your brand&lsquo;s recognition and get detailed insights on how your links are performing.</p>
         <button className="text-white font-bold bg-cyan-500 px-10 py-2 rounded-2xl">Get Started</button>
       </div>
       <div>
